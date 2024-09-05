9ef6aab63cd6edbded69fc28a8a22558582fb0b0
slomec <slomeec@gmail.com>
2024-09-05 07:32:50 +0500

    delete trash in data

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,16 +1,4 @@
-import CollectionCard from "../components/collectionCard";
 import { CollectionCardDataInterface } from "../api/interfaces";
-
-export const paleoCardData: CollectionCardDataInterface = {
-  id: 1,
-  title: "Paleo (Палео)",
-  image: "/paleoImage.png",
-  raiting: 9.0,
-  minPlayers: 4,
-  maxPlayers: 12,
-  duration: 3,
-  difficulty: 2,
-};
 
 export const cards: CollectionCardDataInterface[] = [
   {
